218c512b7bf74011aae7880a01c81ee36d2c3d55
Thiago Cesar <andrade-tc@uol.com.br>
2022-02-16 17:52:42 -0300

    Exercicio Modulo 21

--- exercicios/inteiros.js
+++ exercicios/inteiros.js
@@ -1,14 +1,14 @@
 var inteiros = (a, b) => {
 
-    var x= 0;
+    var x = 0
 
-    for (let i = 1; i < 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
         if(i % a == 0 || i % b == 0){
-            x += i;
-        
-            return x;
+            x += i            
         }  
-}}
+    }
+    return x
+}
 
 module.exports = { inteiros }
 
--- exercicios/inteiros.test.js
+++ exercicios/inteiros.test.js
@@ -4,6 +4,8 @@ describe('Calculo da soma dos numeros inteiros abaixo de 1000', () => {
 
     it('Calculo da soma dos numeros inteiros abaixo de 1000', () => {
         expect(inteiros(5,7)).toBe(156361)
+        expect(inteiros(6,8)).toBe(124502)
+        expect(inteiros(4,9)).toBe(166836)
     });
     
 });
