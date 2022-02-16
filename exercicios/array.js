f570ef753250af5269d5a793881efbd34ab5d014
Thiago Cesar <andrade-tc@uol.com.br>
2022-02-16 20:05:15 -0300

    Solução do Exercicio do Modulo 21

--- exercicios/array.js
+++ exercicios/array.js
@@ -1,15 +1,3 @@
-// var numeros = [11, 23, 3, 14, 5, 66, 37, 48, 19, 1]
-
-// var menor = Math.min.apply(null, numeros)
-// var indMenor = numeros.indexOf(menor)
-// var maior = Math.max.apply(null, numeros)
-// var indMaior = numeros.indexOf(maior)
-
-// console.log("O valor menor é " + menor)
-// console.log("O indice do menor é " + indMenor)
-// console.log("O valor maior é " + maior)
-// console.log("O indice do maior é " + indMaior)
-
 var indMenor = (Array) => {
     var menor = Math.min.apply(null, Array)
     return Array.indexOf(menor)
