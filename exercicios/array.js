bac01958ab53ade9b7cfa9a4c7b8fd12478a356b
Thiago Cesar Andrade <90931093+AndradeTC86@users.noreply.github.com>
2025-01-03 14:43:20 -0300

    Update array.js

--- exercicios/array.js
+++ exercicios/array.js
@@ -1,3 +1,5 @@
+// Dado um array numérico qualquer sem valores repetidos, descubra qual é o índice do maior valor e o índice do menor valor.
+
 var indMenor = (Array) => {
     var menor = Math.min.apply(null, Array)
     return Array.indexOf(menor)
